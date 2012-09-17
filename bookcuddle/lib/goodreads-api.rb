3fcb475e3cfb49def93dbc75645bafa7a74ddf0d
Connie Chen <spiritgreen@gmail.com>
2012-09-16 22:02:12 -0700

    revert api changes

--- bookcuddle/lib/goodreads-api.rb
+++ bookcuddle/lib/goodreads-api.rb
@@ -2,31 +2,27 @@ require 'oauth'
 require 'oauth/consumer'
 
 module GoodReadsAPI
-
-    class GoodReadsAPI
     
-        def im_awesome
-            puts "Connie2 is so awesome"
-        end
-
-        def cool
-            puts "cool!"
-        end
-
-        def authorize_url
-            consumer = OAuth::Consumer.new('oLeXuyhixiL1lMwTsw3fw', 
-                               'Jmw8ybptcmxrxv6p0IyKeTMKSHCPZZ1DYv53bRomU', 
-                               :site => 'http://www.goodreads.com')
-            request_token = consumer.get_request_token
-            puts request_token
-            puts request_token.authorize_url
-            request_token.authorize_url
-        end
-
-        def auth_user
-            uri = URI.parse 'http://www.goodreads.com/api/auth_user'
-        end
+    def GoodReadsAPI.im_awesome
+        puts "Connie2 is so awesome"
     end
 
+    def GoodReadsAPI.cool
+        puts "cool!"
+    end
+
+    def GoodReadsAPI.authorize_url
+        consumer = OAuth::Consumer.new('oLeXuyhixiL1lMwTsw3fw', 
+                           'Jmw8ybptcmxrxv6p0IyKeTMKSHCPZZ1DYv53bRomU', 
+                           :site => 'http://www.goodreads.com')
+        request_token = consumer.get_request_token
+        puts request_token
+        puts request_token.authorize_url
+        request_token.authorize_url
+    end
+
+    def GoodReadsAPI.auth_user
+        uri = URI.parse 'http://www.goodreads.com/api/auth_user'
+    end
 
 end
