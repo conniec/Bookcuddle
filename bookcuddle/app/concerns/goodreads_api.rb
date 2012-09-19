766d2c6bde50c1870af2fa77e90f7b0263dfe7c4
conniec <spiritgreen@gmail.com>
2012-09-19 15:17:26 -0700

    add some error handling to goodreads api calls

--- bookcuddle/app/concerns/goodreads_api.rb
+++ bookcuddle/app/concerns/goodreads_api.rb
@@ -17,22 +17,27 @@ module API
     
     def get_auth_user_goodreads(access_token, access_token_secret)
       res = auth_user(access_token, access_token_secret)
-      doc = Nokogiri::XML(res)
+
+      return {} if res[:code] != '200'
+
+      doc = Nokogiri::XML(res[:data])
 
       goodreads_id = doc.at_xpath("//GoodreadsResponse//user").attr('id')
       goodreads_name = doc.at_xpath("//GoodreadsResponse//name").content
 
-      goodreads_values = {:goodreads_id => goodreads_id, :goodreads_name => goodreads_name}
-      #user = User.get_or_create_goodreads_user(goodreads_id, goodreads_name)
+      {:goodreads_id => goodreads_id, :goodreads_name => goodreads_name}
     end
     
     def get_user_friends(user_id)
       res = user_friends(user_id)
-      doc = Nokogiri::XML(res)
-      
-      friends = doc.xpath("//friends//user")
-      
+
+      puts res[:code] == '200'
       friends_info = []
+      return friends_info if res[:code] != '200'
+
+      puts 'get here?'
+      doc = Nokogiri::XML(res[:data])
+      friends = doc.xpath("//friends//user")
       
       friends.each do |friend|
         info = friend.children
@@ -41,14 +46,7 @@ module API
                           :photo_url => info.at_css('small_image_url').content
                         }
       end
-      
       friends_info
-    end
-
-    def compare_users(goodreads_id)
-      token = set_access_token(@access_token, @access_token_secret)
-      response = token.get("http://www.goodreads.com/user/compare/#{ goodreads_id }.xml")
-      response.body
     end
 
     # def get_user_status_updates(access_token, access_token_secret, goodreads_id)
@@ -74,22 +72,50 @@ module API
       def auth_user(access_token, access_token_secret)
         token = set_access_token(access_token, access_token_secret)
         response = token.get('http://www.goodreads.com/api/auth_user')
-        response.body
+        
+        data = ''
+        if response.code == '200'
+          data = response.body
+        end
+        {:code => response.code, :data => response.body}
       end
-      
       
       def user_info(goodreads_id)
         token = set_access_token(@access_token, @access_token_secret)
         response = token.get("http://www.goodreads.com/user/show/#{ goodreads_id }.xml", { 
                      'key' => 'oLeXuyhixiL1lMwTsw3fw', 
                    })
-        response.body
+
+        data = ''
+        if response.code == '200'
+          data = response.body
+        end
+        {:code => response.code, :data => response.body}
       end
       
       def user_friends(user_id)
         token = set_access_token(@access_token, @access_token_secret)
         response = token.get("http://www.goodreads.com/friend/user/#{ user_id }?format=xml")
-        response.body
+
+        puts 'code is: '
+        puts response.code
+        data = ''
+        if response.code == '200'
+          data = response.body
+        end
+        {:code => response.code, :data => response.body}
       end
+
+      def compare_users(goodreads_id)
+        token = set_access_token(@access_token, @access_token_secret)
+        response = token.get("http://www.goodreads.com/user/compare/#{ goodreads_id }.xml")
+
+        data = ''
+        if response.code == '200'
+          data = response.body
+        end
+        {:code => response.code, :data => response.body}
+      end
+
   end
 end
