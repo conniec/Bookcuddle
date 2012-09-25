56dc1c1581ec28e42b167ec565082a78f4afca26
Ken Shimizu <kenshimizu88@gmail.com>
2012-09-24 21:18:32 -0700

    did nothing

---
+++ bookcuddle/app/models/book.rb
@@ -0,0 +1,10 @@
+class Book
+  include Mongoid::Document
+  field :goodreads_id, type: Integer
+  field :title, type: String
+  field :image_url, type: String
+  field :pub_year, type: String
+  field :description, type: String
+  field :rating, type: String
+  field :num_pages, type: String
+end
